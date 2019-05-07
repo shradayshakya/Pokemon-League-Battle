26687fec12fda7b7658a92687f0dfa17e518e6a7
Shraday Shakya <shradayshakya@gmail.com>
2019-05-07 20:23:56 +0545

    Minor changes

--- js/components/TransitionUtilities.js
+++ js/components/TransitionUtilities.js
@@ -12,29 +12,32 @@ class TransitionUtilities {
     ctx.fillText(
       "Your pokemon is being healed...",
       clientWidth * 0.5 - 150,
-      clientHeight * 0.5,
+      clientHeight * 0.5
     );
-    setTimeout(()=>{
-        this.gameWorldObject.currentState =TILE_WORLD_STATE;
-        this.gameWorldObject.runEngine();
-    },2000);
+    setTimeout(() => {
+      this.gameWorldObject.currentState = TILE_WORLD_STATE;
+      this.gameWorldObject.runEngine();
+    }, 2000);
   }
 
-  nextLevel(){
+  nextLevel() {
     window.cancelAnimationFrame(this.gameWorldObject.mainEngine);
 
     this.gameWorldObject.updateCurrentViewPortAndMap();
 
     let ctx = this.gameWorldObject.ctx;
-    ctx.font = "20px Arial";
-    ctx.fillText(
-      "Next level is being loaded...",
-      clientWidth * 0.5 - 150,
-      clientHeight * 0.5,
+    ctx.globalAlpha = 0.2;
+    ctx.fillStyle = "grey";
+    ctx.fillRect(
+      clientWidth * 0.5 - this.viewPort.width * 0.5,
+      clientHeight * 0.5 - this.viewPort.height * 0.5,
+      this.viewPort.width,
+      this.viewPort.height
     );
-    setTimeout(()=>{
-        this.gameWorldObject.currentState =TILE_WORLD_STATE;
-        this.gameWorldObject.runEngine();
-    },2000);
+    ctx.globalAlpha = 1;
+    setTimeout(() => {
+      this.gameWorldObject.currentState = TILE_WORLD_STATE;
+      this.gameWorldObject.runEngine();
+    }, 2000);
   }
 }
