ffe72cc58942e56417a7b77ff11c62ddba5243d8
Shraday Shakya <shradayshakya@gmail.com>
2019-05-09 21:02:52 +0545

    Door unlock when battle over

--- js/components/TileWorld.js
+++ js/components/TileWorld.js
@@ -36,7 +36,7 @@ class TileWorld{
         else if(mapValue == 98 && !this.gameWorldObject.hasBattleCompleted){
           this.gameWorldObject.currentState = BEFORE_BATTLE_STATE;
         }
-        else if(mapValue == this.pokeMap.exitTile){
+        else if(mapValue == this.pokeMap.exitTile && this.gameWorldObject.hasBattleCompleted){
           this.gameWorldObject.currentState = NEXT_LEVEL_STATE;
         }
       }
